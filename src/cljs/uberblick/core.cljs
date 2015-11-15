1ea1e369607d531fc8bfcd646bbc2b3536c10a24
Max Gerlach <m@px0.de>
2015-11-15 15:52:55 -0500

    lotsa lose pieces!

--- src/cljs/uberblick/core.cljs
+++ src/cljs/uberblick/core.cljs
@@ -16,25 +16,15 @@
     (:import goog.History))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Example data
+(def Max {:IsObjectivesAdmin false, :LaborCategoryID "TECHNCAL", :FirstName "Maximilian", :LaborRoleID "APPLDEVL", :OversightPercent 25, :WorkTeamID "101", :BusinessUnitName "KH4 Tech", :PhotoFileName "4966_2688_sq.jpg", :IsCandidateAdmin false, :CanCommunicateClient false, :UserSystemID 80, :Email "mgerlach@klick.com", :CreatedDate "/Date(1370232000000-0000)/", :BillingTargetHoursPerYear 1650, :Title "Senior Mobile Developer", :IsClient false, :IsScheduleConfirmationRulesEnforced true, :LastName "Gerlach", :IsScheduleAdmin false, :UserName "mgerlach", :Extension "2381", :TimeZoneName "America/Toronto", :IsNotAPerson false, :UserID 4966, :KeyscanUpdated "/Date(1447455535000-0000)/", :HasDirectReports false, :IsAdmin false, :BusinessUnitID 24, :CountryID "CA", :CompanyBusinessUnitID 1, :TimeZoneID 90, :PhotoPath "https://genome.klick.com:443/local-instance/staff%20images/4966_2688_sq.jpg", :Name "Max Gerlach", :Roles [{:RoleID "DWFullAccess", :IsBusinessUnitScope true} {:RoleID "ws", :IsBusinessUnitScope false}], :CompanyBusinessUnitName "Klick Health", :IsWeeklyReviewAdmin false, :Enabled true, :TagName "Max_Gerlach", :Supervisors [3642 3990 4363 3446 3438 3424 3449], :Status {:CanSMS false, :IsOnGenome true, :CanGTalk false}})
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Actions
 
 ;; Network
 (defn- extract-content [] (map #(-> % :body :Entries)))
-
-(defn <get-klickster 
-  [userid]
-  (http/jsonp "https://genome.klick.com:443/api/User.json"
-              {:query-params {"UserID" userid}
-               :channel (chan 1 (extract-content))}))
-
-(defn <search-user 
-  [keyword]
-  (http/jsonp "https://genome.klick.com:443/api/User.json"
-              {:query-params {:ForAutocompleter true
-                              :Enabled true
-                              :IsNotAPerson false
-                              :Keyword keyword}
-               :channel (chan 1 (extract-content))}))
 
 (defn <all-active-users []
   (http/jsonp "https://genome.klick.com:443/api/User/Search"
@@ -42,17 +32,60 @@
 
 (defn extract-userids [users] (map :UserID users))
 
+(defn <users-details [userids]
+  (let [param-string (clojure.string/join "," userids)]
+    (http/jsonp "https://genome.klick.com:443/api/User.json"
+                {:query-params {:UserIDs param-string}
+                 :channel (chan 1 (extract-content))})))
+
+(defn add-full-picturepath
+  [user]
+  (update-in user [:PhotoPath] #(str "https://genome.klick.com:443" (clojure.string/replace %1 #" " "%20"))))
+
+(defn filter-in? [grouped-users]
+  (reduce-kv (fn [m k v]
+               (when (and k
+                          (.startsWith k "In"))
+                 (assoc m k v))) {} grouped-users))
+
 (go
-  (-> (<! (<all-active-users))
-      (extract-userids)
-      prn))
+  (as-> [5702 4966] $
+    (<! (<users-details $))
+    (map add-full-picturepath $)
+    (prn $)))
+
+(go
+  (as-> (<! (<all-active-users)) $
+    (extract-userids $)
+    (drop 200 $)
+    (take 200 $) ; chunk the data!
+    (<! (<users-details $))
+    (group-by :KeyscanStatus $)
+    (filter-in? $)
+    (def people $)
+    (prn $)))
+
+;; TODO:
+;; partition-by :KeyscanStatus. Then display and add filters.
+;; add a <users-details test data set monday morning
+;; partition the users by 200
+
 
 ;; -------------------------
 ;; Views
 
+(defn PersonCard [user]
+  [:div.z-depth-1 {:style {:max-width 100
+                           :text-align :center}}
+   [:div {:style {:width 100
+                  :height 100
+                  :background-image (str "url(" (:PhotoPath user) ")")}}]
+   [:span {:style {:width "100%"
+                   :font-size 16}}
+    (:FirstName user)]])
+
 (defn home-page []
-  [:div [:h2 "Welcome to uberblick!!!"]
-   [:div [:a {:href "/about"} "go to about page"]]])
+  [PersonCard Max])
 
 (defn About
 "Take the readme, render it to HTML, and set it as the element!"
